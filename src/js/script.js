9355faed37de366451fc2490f2a9b4226a67fe1c
pkostovarov <pavel.kostovarov@gmail.com>
2023-01-08 00:40:24 +0100

    08.01.2023

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 
-    
+    // Slaider
+
     $('.slaider__inner').slick({
         speed: 1000, 
         adaptiveHeight: true, 
@@ -20,5 +21,16 @@ $(document).ready(function(){
       
     //маска ввода номера телефона (работает только без type)
     $('input[name=phone]').mask("+38 (099) 99-99-999");
+
+    // Smooth scroll and pageup
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() >1400 ) {
+            $('.pageup').fadeIn('slow');
+        } else {
+            $('.pageup').fadeOut('slow');
+        }
+
+    });
         
 });
